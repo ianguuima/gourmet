8c001a4312614ff45585f28d26a76bef95e3fe53
Ian <ianguuima@gmail.com>
2020-11-25 23:05:18 -0300

    Created README.md base. Let's write a good documentation!

--- backend/src/main/kotlin/me/ianguuima/gourmet/repositories/DishRepository.kt
+++ backend/src/main/kotlin/me/ianguuima/gourmet/repositories/DishRepository.kt
@@ -2,17 +2,13 @@ package me.ianguuima.gourmet.repositories
 
 import me.ianguuima.gourmet.models.Dish
 import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.data.repository.reactive.ReactiveSortingRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 @Repository
 interface DishRepository : ReactiveSortingRepository<Dish, Long> {
 
     fun findByIdNotNull(pageable: Pageable): Flux<Dish>
-
-    //TODO: Fazer o SonicService aqui!
 
 }
