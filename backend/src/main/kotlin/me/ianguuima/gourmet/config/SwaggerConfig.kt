d839a60261b686d6efc6d1c6b55795d198d43139
Ian <ianguuima@gmail.com>
2020-11-26 01:53:24 -0300

    Added documentation

---
+++ backend/src/main/kotlin/me/ianguuima/gourmet/config/SwaggerConfig.kt
@@ -0,0 +1,48 @@
+package me.ianguuima.gourmet.config
+
+import org.reactivestreams.Publisher
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.ResourceHandlerRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+
+@Configuration
+class SwaggerConfig : WebFluxConfigurer {
+
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(ApiInfo(
+                        "Dish API",
+                        "The official Dish API documentation.",
+                        "v1.0",
+                        "./",
+                        Contact("Ian Guimarães", "soon", "ianguuima@gmail.com"),
+                        "MIT",
+                        "https://tldrlegal.com/license/mit-license",
+                        emptyList()
+                ))
+                .genericModelSubstitutes(Mono::class.java, Flux::class.java, Publisher::class.java)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/swagger**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+
+}
--- backend/src/main/kotlin/me/ianguuima/gourmet/controllers/DishController.kt
+++ backend/src/main/kotlin/me/ianguuima/gourmet/controllers/DishController.kt
@@ -1,27 +1,36 @@
 package me.ianguuima.gourmet.controllers
 
+import io.swagger.annotations.Api
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import me.ianguuima.gourmet.models.Dish
 import me.ianguuima.gourmet.services.DishService
 import org.springframework.http.HttpStatus
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import java.util.ArrayList
+import java.util.*
 import javax.validation.Valid
 
-@RestController
+@RestControllerAdvice
 @CrossOrigin
 @RequestMapping(value = ["/dish"])
+@Api(tags = ["Dish"])
 class DishController(
         val dishService: DishService,
 ) {
 
+    @Operation(
+            summary = "Get dish by id",
+    )
     @GetMapping("/{id}")
     fun get(@PathVariable id: Long): Mono<Dish> {
         return dishService.get(id)
     }
 
+    @Operation(
+            summary = "Get all dishes",
+    )
     @GetMapping
     fun findAll(
             @RequestParam(value = "page", required = false, defaultValue = "0") page: Int,
@@ -30,6 +39,10 @@ class DishController(
         return dishService.search(page, size)
     }
 
+    @Operation(
+            summary = "Get word suggestions",
+            description = "This method will search by related words given initial words. Only en-us supported!"
+    )
     @GetMapping("/suggest")
     fun suggest(
             @RequestParam(value = "term", required = true) term : String,
@@ -37,6 +50,11 @@ class DishController(
         return dishService.sonicService.suggest(term)
     }
 
+    @Operation(
+            summary = "Search dishes",
+            description = "You can search for an ingredient." +
+                    " Then, this method will return dishes id that contains this ingredient."
+    )
     @GetMapping("/search")
     fun search(
             @RequestParam(value = "term", required = true) term : String,
@@ -45,6 +63,9 @@ class DishController(
     }
 
 
+    @Operation(
+            summary = "Create a new dish",
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun save(@RequestBody @Valid dish: Dish): Mono<Dish> {
@@ -52,11 +73,17 @@ class DishController(
     }
 
 
+    @Operation(
+            summary = "Update a dish",
+    )
     @PutMapping("/{id}")
     fun update(@PathVariable id: Long, @RequestBody @Valid dish: Dish): Mono<Dish> {
         return dishService.update(dish.copy(id = id))
     }
 
+    @Operation(
+            summary = "Delete a dish",
+    )
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long): Mono<Void> {
         return dishService.delete(id)
